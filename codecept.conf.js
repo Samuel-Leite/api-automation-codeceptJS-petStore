d5aa550cf12852f9a22abcdecdb4015564d972b5
Samuel Leite <slle@gft.com>
2023-03-09 15:19:53 -0300

    add BDD

--- codecept.conf.js
+++ codecept.conf.js
@@ -1,17 +1,4 @@
-const {
-  setHeadlessWhen,
-  setCommonPlugins
-} = require('@codeceptjs/configure');
-// turn on headless mode when running with HEADLESS=true environment variable
-// export HEADLESS=true && npx codeceptjs run
-setHeadlessWhen(process.env.HEADLESS);
-
-// enable all common plugins https://github.com/codeceptjs/configure#setcommonplugins
-setCommonPlugins();
-
-/** @type {CodeceptJS.MainConfig} */
 exports.config = {
-  tests: './test/*_test.js',
   output: './output',
   helpers: {
     REST: {
@@ -21,8 +8,46 @@ exports.config = {
   },
   include: {
     I: './steps_file.js',
-    responseValidation: "./resource/api/responseValidation.js",
-    jsonSchema: "./resource/api/jsonSchema.js"
+    responseValidation: './resource/api/responseValidation.js',
+    jsonSchema: './resource/api/jsonSchema.js'
   },
+  mocha: {},
+  bootstrap: null,
+  timeout: null,
+  teardown: null,
+  hooks: [],
+  gherkin: {
+    features: './features/petStore.feature',
+    steps: ['./step_definitions/petStore_steps.js']
+  },
+  plugins: {
+    screenshotOnFail: {
+      enabled: true
+    },
+    tryTo: {
+      enabled: true
+    },
+    retryFailedStep: {
+      enabled: false
+    },
+    retryTo: {
+      enabled: true
+    },
+    eachElement: {
+      enabled: true
+    },
+    pauseOnFail: {}
+  },
+  stepTimeout: 0,
+  stepTimeoutOverride: [{
+      pattern: 'wait.*',
+      timeout: 0
+    },
+    {
+      pattern: 'amOnPage',
+      timeout: 0
+    }
+  ],
+  // tests: './test/*_test.js',
   name: 'api-codeceptjs'
 }
